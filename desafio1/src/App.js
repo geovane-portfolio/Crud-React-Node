c36e9e1ddf7806471f7103ce799ab81d25457040
Geovane de Sá Ferreira <geovane.portfolio@gmail.com>
2022-01-27 19:03:43 -0300

    Update App.js

--- desafio1/src/App.js
+++ desafio1/src/App.js
@@ -48,44 +48,4 @@ function App() {
 
 export default App;
 
-/*
-function App() {
-  const numeros = [5,3,2,4,7,1,0,6];
-  const listaItems = numeros.sort();
-  
-
-  return (
-    <>
-      <h2>Escrever um código utilizando português estruturado ou Pseudocódigo, no qual o resultado final do Vetor deve conter os mesmos números ordenados crescentemente.
-
-      Considerando o seguinte vetor = (5,3,2,4,7,1,0,6).
-      </h2>
-
-      <p>Lista Ordenada: {listaItems.join(',')}.</p>
-    </>
-  );
-
-} */
-
-
-
-
-/*
-function App() {
-  const vetor = [3,5,1,7,3,9,10];
-
-  function ordenaNum(a,b) {
-    return a - b;
-  }
-
-  vetor.sort(ordenaNum);
-  for(i=0;i<vetor.length;i++) {
-    document.write(vetor[i]+"");
-  }
-
-
-  <p>Lista Ordenada: {vetor[i]+""}.</p>
-}*/
-
-
 
